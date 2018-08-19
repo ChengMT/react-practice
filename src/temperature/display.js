782a0f8bca936d92544ce8c3f97da07a2227bff0
chengmt <cn_chengmt@sina.com>
2018-08-19 21:46:36 +0800

    温度输入的状态提升

--- src/temperature/display.js
+++ src/temperature/display.js
@@ -1,36 +1,35 @@
 import React ,{Component} from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 class Display extends React.Component{
-    static propType = {
-        hua: PropTypes.number,
-        she: PropTypes.number
-    }
+    // static propType = {
+    //     value: PropTypes.number
+    // }
 
     constructor(props){
         super(props);
-        this.state={
-            hua:'',
-            she:''
-        };
+        // this.state={
+        //     hua:'',
+        //     she:''
+        // };
         this.handlehua = this.handlehua.bind(this);
         this.handleshe = this.handleshe.bind(this);
     }
 
-    handlehua(e){
-        this.setState({hua : e.target.value});
+    handlehua(){
+        this.props.onhandlehua;
     }
 
-    handleshe(e){
-        this.setState({she : e.target.value});
+    handleshe(){
+        this.props.onhandleshe;
     }
 
 
     render(){
         return(
             <div>
-                <div>摄氏度：<input value={this.state.she} onChange={this.handleshe}/>℃</div>
-                <div>华氏度：<input value={this.state.hua} onChange={this.handlehua}/>℉</div>
+                <div>摄氏度：<input value={this.props.she} onChange={this.handleshe}/>℃</div>
+                <div>华氏度：<input value={this.props.hua} onChange={this.handlehua}/>℉</div>
             </div>
         )
     }
--- src/temperature/temperature.js
+++ src/temperature/temperature.js
@@ -2,10 +2,30 @@ import React , {Component} from 'react';
 import './temperature.css';
 import Boiling from './boiling.js';
 import Display from './display.js';
+// import PropTypes from 'prop-types';
 
 class Temperature extends React.Component{
+    // static propType = {
+    //     hua: PropTypes.number,
+    //     she: PropTypes.number
+    // }
+
     constructor(props){
         super(props);
+        this.state={
+            hua:"",
+            she:""
+        }
+        this.handlehuaChange = this.handlehuaChange.bind(this);
+        this.handlesheChange = this.handlesheChange.bind(this);
+    }
+
+    handlehuaChange(e){
+        this.setState({hua:e.target.value})
+    }
+
+    handlesheChange(e){
+        this.setState({she:e.target.value})
     }
 
     render(){
@@ -15,7 +35,11 @@ class Temperature extends React.Component{
                     显示水是否已经开了，分别有华氏温度和摄氏温度显示
                 </div>
                 <div>
-                    <Display />
+                    <Display 
+                    onhandlehua={this.handlehuaChange}
+                    onhandleshe={this.handlesheChange}
+                    she={this.props.she}
+                    hua={this.props.hua}/>
                 </div>
                 <div className="content-style">
                     <Boiling CValue='1'/>

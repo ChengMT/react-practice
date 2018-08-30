09b29a6d88ed8ba21776a00d7c94d3f99202512b
chengmt <cn_chengmt@sina.com>
2018-08-30 13:42:43 +0800

    温度转换

--- src/temperature/display.js
+++ src/temperature/display.js
@@ -1,44 +1,33 @@
 import React ,{Component} from 'react';
-// import PropTypes from 'prop-types';
+
+
 
 class Display extends React.Component{
-    // static propType = {
-    //     value: PropTypes.number
-    // }
-
     constructor(props){
         super(props);
-        this.handlehua = this.handlehua.bind(this);
-        this.handleshe = this.handleshe.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
-    // 打开页面对摄氏度的输入框进行聚焦
-    componentDidMount(){
-        this.inputShe.focus()
-    }
-
-    handlehua(){
-        this.props.onhandlehua;
-    }
-
-    handleshe(){
-        this.props.onhandleshe;
+    handleClick(e){
+        this.props.onhandleClick(e.target.value)
     }
 
     render(){
+        const scale = this.props.scale;
+        const temperature = this.props.temperature;
+        const scaleList = {
+            c: 'Celsius摄氏度',
+            f: 'Fahrenheit华氏度'
+        }
         return(
             <div>
-                <div>摄氏度：<input value={this.props.she} onChange={this.handleshe} ref={(input)=>this.inputShe=input} placeholder="此处应输入数字" />℃</div>
-                <div>华氏度：<input value={this.props.hua} onChange={this.handlehua} placeholder="此处应输入数字" />℉</div>
+                请在此输入{scaleList[scale]}：
+                <input value={temperature} onChange={this.handleClick}  />
             </div>
         )
     }
 }
 
-// Display.propType = {
-//     hua: PropTypes.number,
-//     she: PropTypes.number
-// };
 
 export default Display ;
 
--- src/temperature/temperature.js
+++ src/temperature/temperature.js
@@ -5,81 +5,74 @@ import Display from './display.js';
 // import PropTypes from 'prop-types';
 // import Formula from './formula.js';
 
-class Temperature extends React.Component{
-    // static propTypes = {
-    //     hua: PropTypes.number,
-    //     she: PropTypes.number
-    //   }
 
+function toCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+  }
+  
+  function toFahrenheit(celsius) {
+    return (celsius * 9 / 5) + 32;
+  }
+
+  function tryConvert(temperature, convert) {
+    const input = parseFloat(temperature);
+    if (Number.isNaN(input)) {
+        return '';
+    }
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded.toString();
+    
+  }
+
+class Temperature extends React.Component{
     constructor(props){
         super(props);
-        this.state={
-            hua:"",
-            she:""
+        this.state = {
+            temperature:'',
+            scale:'c'
         }
-        this.handlehuaChange = this.handlehuaChange.bind(this);
-        // this.handlesheChange = this.handlesheChange.bind(this);
-        this.ToHua = this.ToHua.bind(this);
-        this.ToShe = this.ToShe.bind(this);
+        this.handleCelsius = this.handleCelsius.bind(this);
+        this.handleFahrenheit =this.handleFahrenheit.bind(this);
     }
 
-    handlehuaChange(e){
-        // var newShe = this.ToShe();
-        // console.log(newShe);
+    handleCelsius(temperature){
         this.setState({
-            hua:e.target.value,
-            // she:newShe
-        });
-        
-
+            scale:'c',
+            temperature
+        })
     }
 
-    // handlesheChange(e){
-    //     var newHua = this.ToHua();
-    //     console.log(newHua);
-    //     this.setState({
-    //         she:e.target.value,
-    //         hua:newHua
-    //     });
-        
-    // }
-
-    // 摄氏度转换为华氏度
-    ToHua(){
-        return (this.state.she * (9/5) + 32);
-    }
-    
-
-    // 华氏温度转换为摄氏温度
-    ToShe(){
-        return ((this.state.hua - 32)* 5/9);
+    handleFahrenheit(temperature){
+        this.setState({
+            scale:'f',
+            temperature
+        })
     }
 
     render(){
+        const temperature = this.state.temperature;
+        const scale = this.state.scale;
+        // const temperatureNewc = <Formula value={temperature} scale='c' />
+        // const temperatureNewf = <Formula value={temperature} scale='f' />
+        const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+        const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
         return(
             <div className="global-style">
                 <div className="instructor">
                     显示水是否已经开了，分别有华氏温度和摄氏温度显示
                 </div>
                 <div>
-                    {/* <Display 
-                    onhandlehua={this.handlehuaChange}
-                    onhandleshe={this.handlesheChange}
-                    she={she}
-                    hua={hua}/> */}
-                    <Display onhandlehua={this.handlehuaChange} hua={this.state.hua}/>
+                    <Display onhandleClick={this.handleCelsius} temperature={celsius} scale='c'/>
+                    <Display onhandleClick={this.handleFahrenheit} temperature={fahrenheit} scale='f'/>
                 </div>
                 <div className="content-style">
-                    <Boiling CValue='1'/>
+                    <Boiling CValue={celsius}/>
                 </div>
             </div>
         )
     }
 }
 
-    // Temperature.propType = {
-    //     hua: PropTypes.number,
-    //     she: PropTypes.number
-    // }
 
 export default Temperature;
